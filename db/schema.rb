4a77276b01d876b1d477ab59f8d6ec0d20d8796b
kongou <oukong84@gmail.com>
2015-08-25 16:31:38 +0700

    Create Musician

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825023527) do
+ActiveRecord::Schema.define(version: 20150825084234) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "venue"
     t.string   "location"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "musicians", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "genre"
+    t.string   "origin"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "image_file_name"
