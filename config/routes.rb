2bce052184c8d3c38e9c19c84f9cdcc193a48e2f
kongou <oukong84@gmail.com>
2015-08-25 17:09:29 +0700

    Fix Conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-root 'home#index'
-
   resources :events
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-=======
->>>>>>> eb4da83e5c1313dfa0585ae07b6c489d08c55ee0
-
   resources :musicians
-  resources :events
   root 'welcome#index'
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150825045019) do
-=======
 ActiveRecord::Schema.define(version: 20150825084234) do
->>>>>>> eb4da83e5c1313dfa0585ae07b6c489d08c55ee0
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -32,7 +28,19 @@ ActiveRecord::Schema.define(version: 20150825084234) do
     t.string   "genre"
   end
 
-<<<<<<< HEAD
+  create_table "musicians", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "genre"
+    t.string   "origin"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -51,19 +59,4 @@ ActiveRecord::Schema.define(version: 20150825084234) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-=======
-  create_table "musicians", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.string   "genre"
-    t.string   "origin"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
->>>>>>> eb4da83e5c1313dfa0585ae07b6c489d08c55ee0
 end
