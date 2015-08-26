d629eebd06985298c1d8309acda7273ed42b0c15
sikheang <ung.sikheang@gmail.com>
2015-08-26 20:48:15 +0700

    Added Origin to Event and add link to navbar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.all.order('created_at DESC')
+  end
+
+  def khmer
+    @events = Event.khmer
+  end
+  def western
+    @events = Event.western
   end
 end
