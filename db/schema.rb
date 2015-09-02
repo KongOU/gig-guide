636942070c02d93f985d90a501019d3fd60d8bdf
sikheang <ung.sikheang@gmail.com>
2015-09-02 11:40:51 +0700

    add field to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901124212) do
+ActiveRecord::Schema.define(version: 20150902042738) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 20150901124212) do
     t.integer  "user_id"
   end
 
-  add_index "events", ["user_id"], name: "index_events_on_user_id"
+  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
 
   create_table "musicians", force: :cascade do |t|
     t.string   "name"
@@ -47,8 +50,8 @@ ActiveRecord::Schema.define(version: 20150901124212) do
     t.integer  "user_id"
   end
 
-  add_index "musicians", ["origin_id"], name: "index_musicians_on_origin_id"
-  add_index "musicians", ["user_id"], name: "index_musicians_on_user_id"
+  add_index "musicians", ["origin_id"], name: "index_musicians_on_origin_id", using: :btree
+  add_index "musicians", ["user_id"], name: "index_musicians_on_user_id", using: :btree
 
   create_table "origins", force: :cascade do |t|
     t.string   "name"
@@ -69,9 +72,13 @@ ActiveRecord::Schema.define(version: 20150901124212) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "events", "users"
+  add_foreign_key "musicians", "origins"
+  add_foreign_key "musicians", "users"
 end
