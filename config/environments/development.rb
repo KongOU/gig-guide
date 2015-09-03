63970ac186ca0bc77583abb3cbc61255441c9955
kongou <oukong84@gmail.com>
2015-09-04 00:08:43 +0700

    Remove username and password to ENV file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,13 +44,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  # domain: "gmail.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: "devbootstrapgig@gmail.com",
-  password: "12345Gig"
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "https://devbootstrap-gigs.herokuapp.com/",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,13 +81,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://devbootstrap-gigs.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "https://devbootstrap-gigs.herokuapp.com/",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: "devbootstrapgig@gmail.com",
-  password: "12345Gig"
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "https://devbootstrap-gigs.herokuapp.com/",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
 
   # Add this line if I get the following error: ActionView::Template::Error: Missing host to link to! Please provide the :host parameter, set default_url_options[:host], or set :only_path to true
