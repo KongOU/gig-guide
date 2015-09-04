ae10e1205b1b590c516e305f7e675399ce3249af
kongou <oukong84@gmail.com>
2015-09-04 10:25:44 +0700

    Add app_secret and app_asscess_token_secret to heroku

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,16 +21,16 @@ class Event < ActiveRecord::Base
   scope :current_event, -> { where('start_on BETWEEN ? AND ?', Date.current.beginning_of_day, Date.current.end_of_day).order(created_at: :DESC).limit(9) }
   scope :yesterday_event, -> { where('start_on BETWEEN ? AND ?', Date.yesterday.beginning_of_day, Date.yesterday.end_of_day).order(created_at: :DESC).limit(9) }
 
-  if Rails.env.development?
-    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png'
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  else
+  # if Rails.env.development?
+  #   has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png'
+  #   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  # else
     has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png',
                       :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  #                     :path => ":style/:id_:filename"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  end
+  # end
   validates :title, :genre, :venue, :location, :origin, presence: true
   validates :description, presence: true, length: {minimum: 50}
 
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -2,16 +2,16 @@ class Musician < ActiveRecord::Base
   belongs_to :origin
   belongs_to :user
 
-  if Rails.env.development?
-    has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: 'default.png'
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  else
+  # if Rails.env.development?
+  #   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: 'default.png'
+  #   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  # else
     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: 'default.png',
                       :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  #                     :path => ":style/:id_:filename"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  end
+  # end
   validates :name, :genre, :origin, presence: true
   validates :description, presence: true, length: {minimum: 20}
 
