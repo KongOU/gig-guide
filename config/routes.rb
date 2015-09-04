31210fcc12ba43d1d3aac57a6e0a89b2a3f770b0
sikheang <ung.sikheang@gmail.com>
2015-09-04 13:23:39 +0700

    Fixed register

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :events do
     collection do
       get 'search'
     end
+    resources :registers
   end
   resources :musicians
   root 'welcome#index'
